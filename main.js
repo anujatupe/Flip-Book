2ec45cad28bae9c227a8e4521bc6e1addd6025b9
Anuja Tupe <anujatupe2012@gmail.com>
2015-04-23 12:55:12 -0700

    Made a book for children - A to Z

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	//var base_z_index = 10;
 	var z_index = 10;
+
 	$('.card').click(function() {
 		if ($(this).hasClass('flip')) {
 			$(this).removeClass('flip');
@@ -9,8 +10,7 @@ $(document).ready(function() {
 		else {
 			z_index = z_index + 1;
 			$(this).css({'z-index' : z_index});
-			$(this).addClass('flip');
-			
+			$(this).addClass('flip');	
 		}
 	});                  
 });
